108c2312c5715f3b0265918ab0f6fb5f87bb362c
Timur <scorpik1992@gmail.com>
2025-07-29 14:44:54 +0600

    isolate ml models on test stage

--- src/ml_model_loader.py
+++ src/ml_model_loader.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import joblib
 import os
+from config import Config
+if Config.Mode == "develop":
+    BASE_DIR = Path(__file__).resolve().parent.parent
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-model = joblib.load(os.path.join(BASE_DIR,"ml_models/model_tree.pkl"))
-model2 = joblib.load(os.path.join(BASE_DIR,"ml_models/model_gradientboosting.pkl"))
+    model = joblib.load(os.path.join(BASE_DIR,"ml_models/model_tree.pkl"))
+    model2 = joblib.load(os.path.join(BASE_DIR,"ml_models/model_gradientboosting.pkl"))
